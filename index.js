8a5fa2053aaf48c28887af26208f3619f2e9adc3
NCesari <nicolascesaripro@gmail.com>
2022-07-29 04:05:38 +0200

    complete remaking done

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 function toggleMenu() {
     const nav = document.querySelector('#nav');
     const burger = document.querySelector('#burger');
+    const header = document.querySelector('#header');
     burger.addEventListener("click", ()=>{
-        nav.classList.toggle('main-header-nav-show')
+        nav.classList.toggle('main-header-nav-show');
+        header.classList.toggle('main-header-show');
     })
 }
 toggleMenu();
